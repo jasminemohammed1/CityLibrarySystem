f39bb2f0771a4c94420fd4e0b2dd5b22bf8b4bee
jasmine <jasminemohamed174005@gmail.com>
2026-05-11 13:11:17 +0300

    Add Property configuration for Author & Category

---
+++ CityLibrarySystem/Configurations/AuthorConfig.cs
@@ -0,0 +1,23 @@
+﻿using CityLibrarySystem.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CityLibrarySystem.Configurations
+{
+    internal class AuthorConfig : IEntityTypeConfiguration<Author>
+    {
+        public void Configure(EntityTypeBuilder<Author> builder)
+        {
+            #region Properties
+            builder.Property(x => x.FirstName).HasColumnType("varchar").HasMaxLength(20);
+            builder.Property(x => x.LastName).HasColumnType("varchar").HasMaxLength(20);
+            #endregion
+
+        }
+    }
+}
---
+++ CityLibrarySystem/Configurations/CategoryConfig.cs
@@ -0,0 +1,24 @@
+﻿using CityLibrarySystem.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CityLibrarySystem.Configurations
+{
+    internal class CategoryConfig : IEntityTypeConfiguration<Category>
+    {
+        public void Configure(EntityTypeBuilder<Category> builder)
+        {
+            #region Properties
+            builder.Property(x => x.Title).HasColumnType("varchar").HasMaxLength(50);
+            builder.Property(x => x.Description).HasColumnType("varchar").HasMaxLength(100);
+            #endregion
+
+
+        }
+    }
+}
